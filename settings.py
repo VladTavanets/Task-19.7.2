1ed178baced24dbb2ea8e3f7a8e7a18c4fcd1f7a
VladTavanets <vtavanets@yandex.ru>
2023-05-28 15:06:17 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,6 @@
+
+valid_email = "piypiy@yaya.ru"
+valid_password = "1234"
+
+# ключ взятый из Swagger для теста
+invalid_auth_key = {"key": "ea738148a1f19838e1c5d1413877f3691a3731380e733e877b0ae729"}
